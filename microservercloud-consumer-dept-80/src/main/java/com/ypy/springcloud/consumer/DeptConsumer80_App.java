cb90dc9946f18b53542ebe6ba1c7722c5ae30c63
Ryan <631811480@qq.com>
2019-09-22 15:30:56 +0800

    自定义负载均衡策略

--- microservercloud-consumer-dept-80/src/main/java/com/ypy/springcloud/consumer/DeptConsumer80_App.java
+++ microservercloud-consumer-dept-80/src/main/java/com/ypy/springcloud/consumer/DeptConsumer80_App.java
@@ -1,13 +1,16 @@
 package com.ypy.springcloud.consumer;
 
+import com.ypy.springcloud.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 //@EnableEurekaClient
 @EnableDiscoveryClient //开启微服务，从注册中心获取服务并调用
+@RibbonClient(name = "MICROSERVERCLOUD-DEPT",configuration = MySelfRule.class)
 public class DeptConsumer80_App {
 
     public static void main(String[] args) {
--- microservercloud-consumer-dept-80/src/main/java/com/ypy/springcloud/myrule/MySelfRule.java
+++ microservercloud-consumer-dept-80/src/main/java/com/ypy/springcloud/myrule/MySelfRule.java
@@ -1,9 +1,6 @@
 package com.ypy.springcloud.myrule;
 
 import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.RandomRule;
-import com.netflix.loadbalancer.RetryRule;
-import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,8 +15,8 @@ public class MySelfRule {
      */
     @Bean
     public IRule myIRule(){
-        // RoundRobinRule 轮询负载均衡算法
-        return new RetryRule();
+//        RoundRobinRule 轮询负载均衡算法
+        return new RandomRule_YPY();
     }
 
 }
